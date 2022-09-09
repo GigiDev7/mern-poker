f22cf0555c1955b9c766300327f7b56842b746ca
GigiDev7 <gigivadachkoria@gmail.com>
2022-09-09 12:40:20 +0400

    added unit test to test straight flush combination

--- client/src/utils/checkCardCombinations.test.tsx
+++ client/src/utils/checkCardCombinations.test.tsx
@@ -1,4 +1,4 @@
-import { checkRoyalFlush } from "./checkCardCombinations";
+import { checkRoyalFlush, checkStraightFlush } from "./checkCardCombinations";
 
 describe("check card combinations utils", () => {
   describe("checking royal flush", () => {
@@ -6,15 +6,6 @@ describe("check card combinations utils", () => {
       const res = checkRoyalFlush(
         ["AH", "KH"],
         ["10H", "QH", "JH", "2S", "3S"]
-      );
-
-      expect(res.isRoyalFlush).toBe(true);
-      expect(res.playerHand).toEqual(["AH", "KH", "QH", "JH", "10H"]);
-    });
-    it("if  royal flush should return it", () => {
-      const res = checkRoyalFlush(
-        ["AH", "10H"],
-        ["KH", "QH", "3S", "2S", "JH"]
       );
 
       expect(res.isRoyalFlush).toBe(true);
@@ -29,13 +20,26 @@ describe("check card combinations utils", () => {
       expect(res.isRoyalFlush).toBe(false);
       expect(res.playerHand).toEqual([]);
     });
-    it("should return royalflush false when its not", () => {
-      const res = checkRoyalFlush(
-        ["AH", "KH"],
-        ["10H", "3S", "JC", "2S", "JH"]
+  });
+
+  describe("checking straight flush", () => {
+    it("should return true if its straight flush", () => {
+      const res = checkStraightFlush(
+        ["10S", "2H"],
+        ["9S", "8S", "AC", "7S", "JS"]
       );
 
-      expect(res.isRoyalFlush).toBe(false);
+      expect(res.isStraightFlush).toBe(true);
+      expect(res.playerHand).toEqual(["JS", "10S", "9S", "8S", "7S"]);
+    });
+
+    it("should return false if its not straight flush", () => {
+      const res = checkStraightFlush(
+        ["10S", "2H"],
+        ["9S", "8S", "AC", "7S", "JH"]
+      );
+
+      expect(res.isStraightFlush).toBe(false);
       expect(res.playerHand).toEqual([]);
     });
   });
